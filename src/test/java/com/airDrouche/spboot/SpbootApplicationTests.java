270dec8002dd09c91e99ffe341d31aa1686d43f4
drouche <drouche.riadh@gmail.com>
2025-02-12 19:21:33 +0100

    init spBoot project

---
+++ src/test/java/com/airDrouche/spboot/SpbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airDrouche.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
